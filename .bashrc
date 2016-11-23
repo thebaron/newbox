6e12c1769bb5513e6389855a34e1bb891f38ce76
Baron Chandler <baron@venturecranial.com>
2016-11-23 00:39:37 -0500

    updated newbox stuff

--- .bashrc
+++
@@ -1,8 +0,0 @@
-
-PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
-export PATH=/usr/local/bin:$PATH
-
-alias vi=vim
-
-
-. $HOME/.mactermrc
---
+++ brew.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+brew install gdbm
+brew install jq
+brew install mas
+brew install node
+brew install oniguruma
+brew install openssl
+brew install packer
+brew install pkg-config
+brew install pwgen
+brew install python
+brew install readline
+brew install sqlite
+brew install terraform
+brew install xhyve
+brew install xz
+brew install youtube-dl
