11647ccffce991f679f04c8657d051f3f5db405a
Baron Chandler <baron@venturecranial.com>
2018-09-03 00:29:55 -0400

    Added some brew updates
    Added a few brews

--- brew.sh
+++ brew.sh
@@ -1,13 +1,19 @@
 #! /bin/sh
 
+brew install ag
+brew install bat
+brew install entr
+brew install fzf
 brew install gdbm
 brew install jq
 brew install mas
 brew install node
+brew install noti
 brew install oniguruma
 brew install openssl
 brew install packer
 brew install pkg-config
+brew install prettyping
 brew install pwgen
 brew install python
 brew install readline
@@ -16,3 +22,6 @@ brew install terraform
 brew install xhyve
 brew install xz
 brew install youtube-dl
+
+brew tap jmespath/jmespath
+brew install jp
