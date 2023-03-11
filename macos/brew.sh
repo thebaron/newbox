e2dbef21620c8463f1cadd33f32671cfe541dfd7
Baron Chandler <baron@venturecranial.com>
2023-03-10 20:43:03 -0500

    refreshed

--- macos/brew.sh
+++ macos/brew.sh
@@ -7,102 +7,93 @@ brew commands 2>&1 > /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubu
 brew update
 brew upgrade
 
-# install a bunch of stuff
-
+# taps
 brew tap jmespath/jmespath
-
-brew install ag \
-             aria2 \
-             bat \
-             cmake \
-             cookiecutter \
-             coreutils \
-             direnv \
-             entr \
-             findutils \
-             fzf \
-             gdbm \
-             gnupg \
-             gnu-sed \
-             gnutls \
-             htop \
-             jq \
-             moreutils \
-             mycli \
-             node \
-             noti \
-             oniguruma \
-             openssh \
-             openssl \
-             p7zip \
-             packer \
-             pgcli \
-             pigz \
-             pkg-config \
-             prettyping \
-             pwgen \
-             pyenv \
-             pyenv-virtualenv \
-             python3 \
-             rclone \
-             readline \
-             screen \
-             sqlite \
-             ssh-copy-id \
-             telnet \
-             terraform \
-             tmux \
-             tree \
-             vim \
-             vips \
-             wget \
-             xz \
-             youtube-dl 
-
-# These don't work yet on M1
-[ $(uname -p) != "arm" ] && brew install \
-             jp \
-             mas 
-
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-ln -s "${HOMEBREW_PREFIX}/bin/gsha256sum" "${HOMEBREW_PREFIX}/bin/sha256sum"
-
-# Install Bash 4.
-brew install bash
-brew install bash-completion2
-
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q "${HOMEBREW_PREFIX}/bin/bash" /etc/shells; then
-  echo "${HOMEBREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-  chsh -s "${HOMEBREW_PREFIX}/bin/bash";
-fi;
-
-# Install font tools.
 brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
 brew tap homebrew/cask-fonts
 
+# standard packages
+PKG=(ag \
+     aria2 \
+     bash \
+     bat \
+     cmake \
+     cookiecutter \
+     coreutils \
+     direnv \
+     entr \
+     findutils \
+     fzf \
+     gdbm \
+     gnupg \
+     gnu-sed \
+     gnutls \
+     htop \
+     jq \
+     jp \
+     mas \
+     moreutils \
+     mycli \
+     node \
+     noti \
+     oniguruma \
+     openssh \
+     openssl \
+     p7zip \ \
+     packer \
+     pgcli \
+     pigz \
+     pkg-config \
+     prettyping \
+     pwgen \
+     pyenv \
+     pyenv-virtualenv \
+     python3 \
+     readline \
+     screen \
+     sqlite \
+     telnet \
+     terraform \
+     tmux \
+     tree \
+     vim \
+     vips \
+     wget \
+     xz \
+    ) 
+
+# extras
+PKG_EXTRAS=(openconnect \
+	    awscli \
+	    go \
+	    go@1.19 \
+	    go@1.18 \
+	    go@1.17 \
+            helm \
+            kubectl \
+            oci-cli \
+            skaffold \
+	    protobuf \
+	    protoc-gen-go \
+	    protoc-gen-go-grpc \
+	    sfnt2woff \
+	    sfnt2woff-zopfli \
+	    woff2 \
+    )
+
+HOMEBREW_NO_INSTALL_CLEANUP=1 brew install ${PKG[@]} ${PKG_EXTRAS[@]}
+
+
 # Install some completions
-brew install brew-cask-completion \
+HOMEBREW_NO_INSTALL_CLEANUP=1 brew install bash-completion2 \
+             brew-cask-completion \
              django-completion \
              pip-completion 
 
-# Install Docker on Intel on Intel on Intel
-[ $(uname -p) != "arm" ] && brew install docker \
-                                         docker-machine \
-                                         docker-machine-driver-xhyve && \
-                            brew services start docker-machine && \
-                            sudo dseditgroup -o create docker && \
-                            sudo dseditgroup -o edit -a thebaron -t user docker && \
-                            sudo chown root:docker ${HOMEBREW_PREFIX}/bin/docker-machine-driver-xhyve && \
-                            sudo chmod u+s         ${HOMEBREW_PREFIX}/bin/docker-machine-driver-xhyve
+# GNU coreutils bins > BSD Mac ones
+GB=$(brew --prefix coreutils)/libexec/gnubin
+grep 'PATH.*libexec.gnubin' ~/.bash_profile > /dev/null || echo "export PATH=\"$GB:$PATH\"" >> ~/.bash_profile
+ln -s "${HOMEBREW_PREFIX}/bin/gsha256sum" "${HOMEBREW_PREFIX}/bin/sha256sum"
 
-
-# Install kubernets-y stuff
-brew install helm \
-             kubectl \
-             skaffold
 # done
 brew cleanup
--- macos/mac-defaults.sh
+++ macos/mac-defaults.sh
@@ -8,13 +8,13 @@ sudo defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool t
 sudo defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
 
 # Enable AirDrop over Ethernet and on unsupported Macs
-defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
-defaults remove com.apple.NetworkBrowser DisableAirDrop
+# defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+# defaults remove com.apple.NetworkBrowser DisableAirDrop
 
 # Disable disk image verification
-defaults write com.apple.frameworks.diskimages skip-verify -bool true
-defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
-defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+# defaults write com.apple.frameworks.diskimages skip-verify -bool true
+# defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+# defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
 
 # Avoid creating .DS_Store files on network volumes
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
@@ -45,7 +45,7 @@ defaults write -g NSDocumentSaveNewDocumentsToCloud -bool false
 
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 defaults write com.apple.helpviewer DevMode -bool true
-defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+# defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
 
 ###############################################################################
@@ -224,16 +224,16 @@ defaults write com.apple.Safari InstallExtensionUpdatesAutomatically -bool true
 ###############################################################################
 
 # Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
-defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+# defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 
 # Display emails in threaded mode, sorted by date (oldest at the top)
-defaults write com.apple.mail DraftsViewerAttributes -dict-add "DisplayInThreadedMode" -string "yes"
-defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortedDescending" -string "yes"
-defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortOrder" -string "received-date"
+# defaults write com.apple.mail DraftsViewerAttributes -dict-add "DisplayInThreadedMode" -string "yes"
+# defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortedDescending" -string "yes"
+# defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortOrder" -string "received-date"
 
 # Disable inline attachments (just show the icons)
-defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
-
+# defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
+# 
 ###############################################################################
 # Spotlight                                                                   #
 ###############################################################################
@@ -286,8 +286,8 @@ sudo mdutil -E / > /dev/null
 ## Terminals
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
-open "colorschemes/*.itermcolors"
-open "colorschemes/*.terminal"
+# open "colorschemes/*.itermcolors"
+# open "colorschemes/*.terminal"
 
 ###############################################################################
 # Time Machine                                                                #
@@ -349,37 +349,3 @@ defaults write com.google.Chrome DisablePrintPreview -bool true
 # Expand the print dialog by default
 defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
 
-###############################################################################
-# Transmission.app                                                            #
-###############################################################################
-
-# Use `~/Documents/Torrents` to store incomplete downloads
-defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
-defaults write org.m0k.transmission IncompleteDownloadFolder -string "${HOME}/Documents/Torrents"
-
-# Use `~/Downloads` to store completed downloads
-defaults write org.m0k.transmission DownloadLocationConstant -bool true
-
-# Don’t prompt for confirmation before downloading
-defaults write org.m0k.transmission DownloadAsk -bool false
-defaults write org.m0k.transmission MagnetOpenAsk -bool false
-
-# Don’t prompt for confirmation before removing non-downloading active transfers
-defaults write org.m0k.transmission CheckRemoveDownloading -bool true
-
-# Trash original torrent files
-defaults write org.m0k.transmission DeleteOriginalTorrent -bool true
-
-# Hide the donate message
-defaults write org.m0k.transmission WarningDonate -bool false
-# Hide the legal disclaimer
-defaults write org.m0k.transmission WarningLegal -bool false
-
-# IP block list.
-# Source: https://giuliomac.wordpress.com/2014/02/19/best-blocklist-for-transmission/
-defaults write org.m0k.transmission BlocklistNew -bool true
-defaults write org.m0k.transmission BlocklistURL -string "http://john.bitsurge.net/public/biglist.p2p.gz"
-defaults write org.m0k.transmission BlocklistAutoUpdate -bool true
-
-# Randomize port on launch
-defaults write org.m0k.transmission RandomPort -bool true
